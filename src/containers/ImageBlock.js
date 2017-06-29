d879eeb4602d6784256d7a87ba2286b8d5193915
pawankeer <pawandeep.keer@gmail.com>
2017-06-29 08:08:19 -0400

    separated the search portion into its own component. and minor ui improvements

--- src/containers/ImageBlock.js
+++ src/containers/ImageBlock.js
@@ -7,15 +7,15 @@ class ImageBlock extends Component {
         switch (this.props.imgSize) {
             case "medium":
                 return <div className="imageGridMedium">
-                    <img src={this.props.img} alt="Img not available" />
+                    <img src={this.props.img} alt="Img not available" width="100%" />
                 </div>;
             case "large":
                 return <div className="imageGridLarge">
-                    <img src={this.props.img} alt="Img not available" />
+                    <img src={this.props.img} alt="Img not available" width="100%" />
                 </div>;
             default:
                 return <div className="imageGridSmall">
-                    <img src={this.props.img} alt="Img not available" />
+                    <img src={this.props.img} alt="Img not available" width="100%" />
                 </div>;
         }
     }
