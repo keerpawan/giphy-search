63cfee8472af5d3cbfef9e2f802525ff9d2ebce6
pawankeer <pawandeep.keer@gmail.com>
2017-06-29 08:28:06 -0400

    comments on service

--- src/services/GiphyService.js
+++ src/services/GiphyService.js
@@ -1,10 +1,11 @@
 import axios from '../../node_modules/axios/dist/axios';
 
-
+// this function returns a promise
 function list(tags, page, limit) {
     return axios.get(`http://api.giphy.com/v1/gifs/search?q=${encodeURI(tags.replace("#", ""))}&api_key=dc6zaTOxFJmzC&limit=${limit}&offset=${page * 25}`)
 }
 
+// service to connect to giphy's apis. we can add more apis in the future.
 export default function GiphyService() {
     return {
         list: list
